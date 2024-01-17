b841ab9af09a34a8af383301a931596332f575fe
RelexSun <88622289+RelexSun@users.noreply.github.com>
2024-01-17 14:04:03 +0700

    aos animation

--- src/components/sections/developer.tsx
+++ src/components/sections/developer.tsx
@@ -1,49 +1,15 @@
 "use client";
 
-import { useEffect, useRef } from "react";
-import { Button } from "@/components/ui/button";
-import {
-  motion,
-  useInView,
-  useAnimation,
-  AnimatePresence,
-} from "framer-motion";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-const animation = {
-  hidden: { opacity: 0, y: 75 },
-  visible: { opacity: 1, y: 0 },
-};
+import { Button } from "@/components/ui/button";
 
 const DeveloperSection = () => {
-  const ref1 = useRef(null);
-  const ref2 = useRef(null);
-  const ref3 = useRef(null);
-
-  const isInView1 = useInView(ref1);
-  const isInView2 = useInView(ref2);
-  const isInView3 = useInView(ref3);
-
-  const controls1 = useAnimation();
-  const controls2 = useAnimation();
-  const controls3 = useAnimation();
-
   useEffect(() => {
-    if (isInView1) {
-      controls1.start("visible");
-    }
-  }, [isInView1]);
-
-  useEffect(() => {
-    if (isInView2) {
-      controls2.start("visible");
-    }
-  }, [isInView2]);
-
-  useEffect(() => {
-    if (isInView3) {
-      controls3.start("visible");
-    }
-  }, [isInView3]);
+    AOS.init();
+  }, []);
 
   return (
     <div className="mt-16 grid gap-20 ">
@@ -57,14 +23,7 @@ const DeveloperSection = () => {
             voluptatem cumque facilis! Quod, nisi?
           </p>
         </div>
-        <motion.div
-          ref={ref1}
-          variants={animation}
-          initial="hidden"
-          animate={controls1}
-          transition={{ duration: 1, delay: 0.25 }}
-          className="img w-full h-60 bg-cyan-500"
-        ></motion.div>
+        <div className="img w-full h-60 bg-cyan-500" data-aos="zoom-out"></div>
       </div>
       <div className="flex flex-col sm:flex-row gap-10">
         <div className="w-full">
@@ -76,15 +35,9 @@ const DeveloperSection = () => {
             voluptatem cumque facilis! Quod, nisi?
           </p>
         </div>
-        <motion.div
-          ref={ref2}
-          variants={animation}
-          initial="hidden"
-          animate={controls2}
-          transition={{ duration: 1, delay: 0.25 }}
-          className="img w-full h-60 bg-cyan-500"
-        ></motion.div>
+        <div className="img w-full h-60 bg-cyan-500" data-aos="zoom-out"></div>
       </div>
+
       <div className="flex flex-col sm:flex-row-reverse gap-10">
         <div className="w-full">
           <h1 className="text-xl font-bold">Ready for Developers</h1>
@@ -99,14 +52,7 @@ const DeveloperSection = () => {
           </Button>
         </div>
 
-        <motion.div
-          ref={ref3}
-          variants={animation}
-          initial="hidden"
-          animate={controls3}
-          transition={{ duration: 1, delay: 0.25 }}
-          className="img w-full h-60 bg-cyan-500"
-        ></motion.div>
+        <div className="img w-full h-60 bg-cyan-500" data-aos="zoom-out"></div>
       </div>
     </div>
   );
--- src/components/sections/solution.tsx
+++ src/components/sections/solution.tsx
@@ -2,8 +2,8 @@ import { Button } from "@/components/ui/button";
 
 const SolutionSection = () => {
   return (
-    <div className="bg-slate-500 w-full flex flex-col items-center">
-      <div className="bg-red-500 w-full h-[855px] border"></div>
+    <div className="bg-slate-500 w-full flex flex-col items-center sm:flex-row-reverse">
+      <div className="bg-red-500 w-full h-[350px] border"></div>
       <div className="w-full">
         <h1 className="text-[30px] font-bold">Payment Solution</h1>
         <div>
